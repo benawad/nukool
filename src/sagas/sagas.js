d5ac30d573525bf68287a9445046482e1158c266
ben awad <benawad97@gmail.com>
2016-12-25 15:14:21 -0600

    config for gh-pages

--- src/sagas/sagas.js
+++ src/sagas/sagas.js
@@ -1,11 +1,11 @@
 import { takeEvery } from 'redux-saga';
 import { fork, call, put } from 'redux-saga/effects';
-import { browserHistory } from 'react-router';
+import { hashHistory } from 'react-router';
 import request from 'superagent';
 
 function sendMessages(payload) {
   return request
-    .post('http://localhost:5000')
+    .post('https://nukool-server.herokuapp.com')
     .send(
       payload
     )
@@ -14,10 +14,10 @@ function sendMessages(payload) {
 }
 
 function* callSendMessages(action) {
-  browserHistory.push('/wait');
+  hashHistory.push('/wait');
   const result = yield call(sendMessages, action);
   yield put({type: 'SEND_MESSAGES_DONE', result});
-  browserHistory.push('/result');
+  hashHistory.push('/result');
 }
 
 function* sendMessagesSaga() {
